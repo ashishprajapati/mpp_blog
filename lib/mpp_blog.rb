1696fbda802cfb8c3cacdb2005eb615ab6530d82
Ashish Prajapati <ashishprajapati0404@gmail.com>
2015-12-09 12:50:46 +0530

    Update mpp_blog.rb

--- lib/mpp_blog.rb
+++ lib/mpp_blog.rb
@@ -1,4 +1,15 @@
 require "mpp_blog/engine"
-
+require 'font-awesome-rails'
+require 'haml-rails'
+require 'sass-rails'
+require 'bootstrap-sass-rails'
+require 'coffee-rails'
+require 'jquery-rails'
+require 'mini_magick'
+require 'open_uri_redirections'
+require 'acts-as-taggable-on'
+require 'truncate_html'
+require 'carrierwave'
+require 'kaminari'
 module MppBlog
 end
